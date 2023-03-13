440420c3ad578b65bc9c3156b150f1d902fd9779
lecoqm <125046145+lecoqm@users.noreply.github.com>
2023-03-13 22:50:14 +0100

    Update test_s1q4_graph_loading2.py

--- test/test_s1q4_graph_loading2.py
+++ test/test_s1q4_graph_loading2.py
@@ -1,1 +1,18 @@
-# À compléter
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network")
+
+from graph import Graph, graph_from_file
+
+import unittest   # The test framework
+
+input="/home/onyxia/projet_ENSAE_1A/input/"
+
+class Test_Reachability(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file(input+"network.04.in")
+        self.assertEqual(sorted(g.graph[2],key=lambda x:x[0]), [[1, 4, 89],[3, 4, 3]])
+        self.assertEqual(sorted(g.graph[3],key=lambda x:x[0]), [[2, 4, 3], [4, 4, 2]])
+
+if __name__ == '__main__':
+    unittest.main()
