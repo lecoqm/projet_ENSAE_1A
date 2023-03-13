e2f01d97a2981785ae19e66144273878c2bd0130
lecoqm <125046145+lecoqm@users.noreply.github.com>
2023-03-13 22:56:04 +0100

    Update test_s1q5_min_distance.py

--- test/test_s1q5_min_distance.py
+++ test/test_s1q5_min_distance.py
@@ -1,1 +1,18 @@
-# À compléter
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network")
+
+from graph import Graph, graph_from_file
+
+import unittest   # The test framework
+
+input="/home/onyxia/projet_ENSAE_1A/input/"
+
+class Test_Reachability(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file(input+"network.04.in")
+        self.assertEqual(g.get_path_with_power(1, 4, 4), [1, 2, 3, 4])
+        self.assertEqual(g.get_path_with_power(1, 4, 89), [1,4])
+
+if __name__ == '__main__':
+    unittest.main()
