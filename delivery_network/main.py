b975fc166837ec90825c0b49e99a0a50bf8fab82
lecoqm <125046145+lecoqm@users.noreply.github.com>
2023-03-13 22:48:21 +0100

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,12 @@
 from graph import Graph, graph_from_file
+from kruskal import kruskal, union, find
 
-
-data_path = "input/"
-file_name = "network.01.in"
+data_path = "/home/onyxia/projet_ENSAE_1A/input/"
+file_name = "network.04.in"
 
 g = graph_from_file(data_path + file_name)
-print(g)
+#print(g)
+
+a=kruskal(g)
+b=a.kpath(2,4)
+print(b)
